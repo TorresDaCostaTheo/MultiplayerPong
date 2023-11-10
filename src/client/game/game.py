37bac56804e3f840f65fc57ebf07bdb687f1e935
CARLU <antoine.carlu@outlook.fr>
2023-11-10 14:35:19 +0100

    Classe ecran

--- src/client/game/game.py
+++ src/client/game/game.py
@@ -1,86 +1,105 @@
 import pygame
+import pygame_menu
 from Const import *
 from Striker import Striker
 from Ball import Ball
+from ecran import Ecran
 
+# Initialisation de Pygame
+pygame.init()
 
-# Game Manager
+# Initialisation de l'écran
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong Game")
+
+# Initialisation de l'horloge
+clock = pygame.time.Clock()
 
 def main():
-	running = True
+    running = True
 
-	# Defining the objects
-	player1 = Striker(20, 0, 10, 100, 10, GREEN)
-	player2 = Striker(WIDTH-30, 0, 10, 100, 10, RED)
-	ball = Ball(WIDTH//2, HEIGHT//2, 7, 7, WHITE)
+    # Définir les objets
+    player1 = Striker(20, 0, 10, 100, 10, GREEN)
+    player2 = Striker(WIDTH-30, 0, 10, 100, 10, RED)
+    ball = Ball(WIDTH//2, HEIGHT//2, 7, 7, WHITE)
+    ecran = Ecran(900, 650, '', 400, 300, pygame_menu.themes.THEME_DARK)
 
-	listOfplayers = [player1, player2]
+    listOfplayers = [player1, player2]
 
-	# Initial parameters of the players
-	player1Score, player2Score = 0, 0
-	player1YFac, player2YFac = 0, 0
+    # Initial parameters of the players
+    player1Score, player2Score = 0, 0
+    player1YFac, player2YFac = 0, 0
 
-	while running:
-		screen.fill(BLACK)
+    while running:
+        screen.fill(BLACK)
 
-		# Event handling
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_UP:
-					player2YFac = -1
-				if event.key == pygame.K_DOWN:
-					player2YFac = 1
-				if event.key == pygame.K_z:
-					player1YFac = -1
-				if event.key == pygame.K_s:
-					player1YFac = 1
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-					player2YFac = 0
-				if event.key == pygame.K_z or event.key == pygame.K_s:
-					player1YFac = 0
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player2YFac = -1
+                if event.key == pygame.K_DOWN:
+                    player2YFac = 1
+                if event.key == pygame.K_z:
+                    player1YFac = -1
+                if event.key == pygame.K_s:
+                    player1YFac = 1
+                if event.key == pygame.K_ESCAPE:
+                    ecran.set_menu_title('Pause')
+                    ecran.setup_menus_pause()
+                    ecran.run()
+                    
 
-		# Collision detection
-		for player in listOfplayers:
-			if pygame.Rect.colliderect(ball.getRect(), player.getRect()):
-				ball.hit()
 
-		# Updating the objects
-		player1.update(player1YFac)
-		player2.update(player2YFac)
-		point = ball.update()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    player2YFac = 0
+                if event.key == pygame.K_z or event.key == pygame.K_s:
+                    player1YFac = 0
 
-		# -1 -> player_1 has scored
-		# +1 -> player_2 has scored
-		# 0 -> None of them scored
-		if point == -1:
-			player1Score += 1
-		elif point == 1:
-			player2Score += 1
+        # Collision detection
+        for player in listOfplayers:
+            if pygame.Rect.colliderect(ball.getRect(), player.getRect()):
+                ball.hit()
 
-		# Someone has scored
-		# a point and the ball is out of bounds.
-		# So, we reset it's position
-		if point: 
-			ball.reset()
+        # Updating the objects
+        player1.update(player1YFac)
+        player2.update(player2YFac)
+        point = ball.update()
 
-		# Displaying the objects on the screen
-		player1.display()
-		player2.display()
-		ball.display()
+        # -1 -> player_1 has scored
+        # +1 -> player_2 has scored
+        # 0 -> None of them scored
+        if point == -1:
+            player1Score += 1
+        elif point == 1:
+            player2Score += 1
 
-		# Displaying the scores of the players
-		player1.displayScore("player_1 : ", 
-						player1Score, 100, 20, WHITE)
-		player2.displayScore("player_2 : ", 
-						player2Score, WIDTH-100, 20, WHITE)
+        # Someone has scored
+        # a point and the ball is out of bounds.
+        # So, we reset its position
+        if point: 
+            ball.reset()
+        
+        if player1Score == 10 or player2Score == 10 :
+             ecran.set_menu_title('Fin')
+             ecran.setup_menus_fin()
+             ecran.run()
+                
+        # Displaying the objects on the screen
+        player1.display()
+        player2.display()
+        ball.display()
 
-		pygame.display.update()
-		clock.tick(FPS)	 
+        # Displaying the scores of the players
+        player1.displayScore("player_1 : ", player1Score, 100, 20, WHITE)
+        player2.displayScore("player_2 : ", player2Score, WIDTH-100, 20, WHITE)
 
+        pygame.display.update()
+        clock.tick(FPS)
 
 if __name__ == "__main__":
-	main()
-	pygame.quit()
+    main()
+    pygame.quit()
