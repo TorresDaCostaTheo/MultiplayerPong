437e6016e32017d6d126da031f2747b873833819
CARLU <antoine.carlu@outlook.fr>
2023-11-20 09:14:35 +0100

    menu synchro fin

--- src/client/game/ecran.py
+++ src/client/game/ecran.py
@@ -20,10 +20,10 @@ class Ecran:
         self.menu_title = new_title
         self.menu.set_title(new_title)
 
-    def setup_menus_Attente(self, instance_jeu):
+    def setup_menus_attente(self, instance_jeu):
         self.menu.clear()
         # Ajoutez du contenu au menu
-        self.menu.add.label('Attendre le second joueur', font_size=20, margin=(0, 20))
+        self.menu.add.label('Attente du second joueur', font_size=20, margin=(0, 20))
 
         name_player2=self.return_nom_joueur2()
         if name_player2 != "":
@@ -41,6 +41,12 @@ class Ecran:
 
     def display_pause(self,instance_jeu):
         self.joueur.send_data_game(self.joueur.username, instance_jeu.player1YFac, False)
+        self.set_menu_title("Attente")
+        self.setup_verification_pause(instance_jeu)
+        self.run("attente pause")
+
+    def setup_verification_pause(self,instance_jeu):
+        self.menu.clear()
         pause=self.return_pause()
     
         if pause==False:
@@ -57,6 +63,12 @@ class Ecran:
 
     def display_fin(self,instance_jeu):
         self.joueur.send_data_game(self.joueur.username, instance_jeu.player1YFac, False)
+        self.set_menu_title("Attente")
+        self.setup_verification_fin(instance_jeu)
+        self.run("attente fin")
+
+    def setup_verification_fin(self,instance_jeu):
+        self.menu.clear()
         pause=self.return_pause()
     
         if pause==False:
@@ -69,13 +81,18 @@ class Ecran:
             for event in events:
                 if current_state == "attente":
                     if event.type == USEREVENT:
-                        self.setup_menus_Attente(event.instance_jeu)
-                        self.setup_menus_pause(event.instance_jeu)
-                    else:
+                        self.setup_menus_attente(event.instance_jeu)
+                if current_state == "attente pause":
+                    if event.type == USEREVENT:
+                        self.setup_verification_pause(event.instance_jeu)
+                if current_state == "attente fin":
+                    if event.type == USEREVENT:
+                        self.setup_verification_fin(event.instance_jeu)
+                if current_state != "attente" and current_state != "attente pause" and current_state != "attente fin":
                         if event.type == pygame.QUIT:
                             self.running=False
                         
-            if current_state == "attente":
+            if current_state == "attente" and current_state == "attente pause" and current_state == "attente fin":
                 # Mettez à jour les menus
                 self.menu.update([])
             else:
@@ -102,6 +119,9 @@ class Ecran:
     def update_pause(self, pause,instance_jeu):
         global global_pause
         global_pause = pause  # Mettre à jour la variable globale
+        pygame.event.post(self.event_queue)
+        # Ajoutez l'instance_jeu comme attribut de l'événement
+        self.event_queue.instance_jeu = instance_jeu
 
     def return_pause(self):
         global global_pause
